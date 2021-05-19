9763c4e3a68eb4427c766a99560afeacdfc883fc
FosterChad95 <fosterchad95@gmail.com>
2021-05-19 14:07:51 -0400

    First Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import logo from "../../assets/shops.png";
+import useStyles from "./styles";
+
+import { ShoppingCart } from "@material-ui/icons";
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Shopity
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart Items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/lib/commerce.js
@@ -0,0 +1,5 @@
+import Commerce from "@chec/commerce.js";
+
+export const commerce = new Commerce(
+  "pk_2765353a4adfd42a887cbf9f25a66119874bc10cb4233"
+);
